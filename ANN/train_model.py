57401725b6f6ee15c23a1d379040861393c3cd09
Gaëtan <gaetan.serre@universite-paris-saclay.fr>
2021-05-17 10:56:41 +0200

    maj notebooks

--- ANN/train_model.py
+++ ANN/train_model.py
@@ -45,31 +45,24 @@ def save_loss(history):
 
 
 def saveTFModel(model, output_path):
-    s = ""
-    for layer in model.layers:
-        w = layer.get_weights()
-        if len(w) > 1:
-            s += f"layer {layer.get_config()['activation']}\n"
-            weights = w[0]
-            bias = w[1]
-
-            dimensions = len(weights)
-            nb_neurons = len(weights[0])
-
-            if nb_neurons == 1:
-                for i in range(dimensions):
-                    s += str(weights[i][0]) + " "
-                s += str(bias[0]) + "\n"
-
-            else:
-                for i in range(nb_neurons):
-                    w = []
-                    for j in range(dimensions):
-                        s += str(weights[j][i]) + " "
-                    s += str(bias[i]) + "\n"
-    f = open(output_path, "w")
-    f.write(s)
-    f.close()
+  s = ""
+  for layer in model.layers:
+    w = layer.get_weights()
+    if len(w) > 1:
+      s += f"layer {layer.get_config()['activation']}\n"
+      weights = w[0]
+      bias = w[1]
+      
+      dimensions = len(weights)
+      nb_neurons = len(weights[0])
+      for i in range(nb_neurons):
+        for j in range(dimensions):
+        	s += str(weights[j][i]) + " "
+      	s+= str(bias[i]) + "\n"
+        
+  f = open(output_path, "w")
+  f.write(s)
+  f.close()
 
 
 rebuild = True
